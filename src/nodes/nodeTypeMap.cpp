6c4e8f4c100daa8842145a74858c9eff14624480
mike <mda5893@rit.edu>
2019-11-11 20:08:04 -0700

    Added bionic to ubuntu containers

--- src/nodes/nodeTypeMap.cpp
+++ src/nodes/nodeTypeMap.cpp
@@ -3,6 +3,7 @@
 
 const std::map<std::string, NodeType*> nodeTypeMap = {
     {"ubuntu xenial", new LxcContainer("ubuntu", "xenial")}, 
+    {"ubuntu bionic", new LxcContainer("ubuntu", "bionic")}, 
     {"archlinux current", new LxcContainer("archlinux", "current")}, 
     {"fedora 25", new LxcContainer("fedora", "25")},
 };
